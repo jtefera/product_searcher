f6f097cda921bf72778e9583588447c99c2c28b6
Jonathan Tefera <jonathantefera@outlook.com>
2017-01-08 23:36:11 -0800

    Changed Webpack to load also jsx files

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = [{
     module: {
         loaders: [
             {
-                test: /.js$/,
+                test: /.jsx?$/,
                 loader: 'babel-loader',
                 exclude: /node_modules/,
                 query: {
