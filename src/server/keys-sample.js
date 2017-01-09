4387cc275d368c020ca476ff5d02f797d7034d7d
Jonathan Tefera <jonathantefera@outlook.com>
2017-01-08 23:33:13 -0800

    Basic Resful api on the server.

---
+++ src/server/keys-sample.js
@@ -0,0 +1,11 @@
+// This file is just a sample
+// You should change each of the next values with your keys
+// After that, rename this file as keys.js
+
+/* Outpan key: from outpan.com 
+    Outpan is an organization that provides an rest api
+    that receives as input a barcode key and
+    returns a json object with information of product 
+    for that barcode
+*/
+export const outpanKey = "your outpan key";
--- src/server/server.js
+++ src/server/server.js
@@ -3,6 +3,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
 import morgan from 'morgan';
+import {polyfill} from 'es6-promise';
+import fetch from 'isomorphic-fetch';
+import {outpanKey} from './keys';
+polyfill();
+
 
 // Server
 let app = express();
@@ -18,6 +23,13 @@ app.use(bodyParser.json());
 
 const ServerPort = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 const ServerIpAddress = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+app.get('/get_product/:barcode', (req,  res) => {
+    const apiKey = outpanKey;
+    var url = `https://api.outpan.com/v2/products/${req.params.barcode}?apikey=${apiKey}`;
+    fetch(url)
+        .then((apiRes) => apiRes.json())
+        .then(res.json.bind(res))
+});
 
 app.listen(ServerPort, ServerIpAddress, () => {
 	console.log('dirname is ' + __dirname);
